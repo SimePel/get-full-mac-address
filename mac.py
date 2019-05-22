27cf3d11549268b0f9081a6f4ec253b227690997
SimePel <simepeldev@gmail.com>
2019-05-22 12:35:34 +0700

    Rewrite with netmiko

--- mac.py
+++ mac.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python
-
+from netmiko import Netmiko
 import sys
 import os
 
-import paramiko
-
 def get_full_mac() -> str:
-    username = os.getenv("CISCO_LOGIN")
-    password = os.getenv("CISCO_PASS")
-    campus_switches = {"m" : "sw-102-0-mc.noc.asu.ru", "l" : "sw-104-1-lc.noc.asu.ru",
-                    "d" : "sw-024-1-dc.noc.asu.ru", "s" : "sw-102-1-sc.noc.asu.ru",
-                    "k" : "sw-403-1-kc.noc.asu.ru"}
+    campus_switches = {"m": "sw-102-0-mc.noc.asu.ru", "l": "sw-104-1-lc.noc.asu.ru",
+                       "d": "sw-024-1-dc.noc.asu.ru", "s": "sw-102-1-sc.noc.asu.ru",
+                       "k": "sw-403-1-kc.noc.asu.ru"}
 
-    if len(sys.argv) != 3:
-        print("python3 mac.py [short mac] [letter of campus(m,l,d,s,k)]")
+    switch = campus_switches["m"]
+    if len(sys.argv) == 3:
+        switch = campus_switches[sys.argv[2]]
+    if len(sys.argv) == 1:
+        print("./mac.py [short mac] [letter of campus(m,l,d,s,k)]")
         sys.exit(1)
 
-    mac = sys.argv[1]
-    switch = campus_switches[sys.argv[2]]
+    cisco = Netmiko(
+        switch,
+        username=os.getenv("CISCO_LOGIN"),
+        password=os.getenv("CISCO_PASS"),
+        device_type="cisco_ios",
+    )
 
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.MissingHostKeyPolicy())
-    client.connect(hostname=switch, username=username, password=password, allow_agent=False, look_for_keys=False)
-    
-    _, stdout, _ = client.exec_command('show mac address-table | include ' + mac)
-    data = stdout.read().decode('utf-8')
-    if data == '':
+    mac = sys.argv[1]
+    output = cisco.send_command('show mac address-table | include ' + mac)
+    if output == '':
         return 'Мак адрес не найден'
 
-    return data.split()[1]
+    return output.split()[1]
 
 if __name__ == "__main__":
     print(get_full_mac())
